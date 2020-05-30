c35b266530631cd280aa75590446fa03097a75ef
shrushti2000 <shrushtipolekar@gmail.com>
2020-05-30 15:41:26 +0530

    Updates

---
+++ precache-manifest.66de46a164434f17a03a9e2a138f4996.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16a636ac66fc813afcf4e885168f8c74",
+    "url": "/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "46cde771cae76a5535a7",
+    "url": "/Monster-Rolodex/static/css/main.3d4a2a86.chunk.css"
+  },
+  {
+    "revision": "ef6aa4c9c0b65164e432",
+    "url": "/Monster-Rolodex/static/js/2.e84d26c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-Rolodex/static/js/2.e84d26c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46cde771cae76a5535a7",
+    "url": "/Monster-Rolodex/static/js/main.11d7111f.chunk.js"
+  },
+  {
+    "revision": "39754c773e1d7fb81761",
+    "url": "/Monster-Rolodex/static/js/runtime-main.b0e90f1c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Monster-Rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
